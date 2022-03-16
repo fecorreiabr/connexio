590fc3ced4cc5eec803f3da7ec44ef6fb51a0fbc
fecorreiabr <felipe.reis.correia@gmail.com>
2022-03-16 11:43:45 -0300

    PixiNode and PixiLink components creation

---
+++ src/components/pixi-link.ts
@@ -0,0 +1,198 @@
+import { GraphConfig } from "@/options";
+import { Graph, Layout, Link, Vector } from "@/plugins/ngraph";
+import { Graphics, Polygon } from "@/plugins/pixijs";
+
+interface Edge {
+    position: {
+        from: Vector;
+        to: Vector;
+    };
+    arrow? : {
+        start: Vector;
+        nodes: Vector[];
+    };
+}
+
+export class PixiLink extends Graphics {
+    id: string;
+    // histArea: Polygon;
+    sourceLinks: string[];
+    edges: Record<string, Edge>;
+    fromPos?: Vector;
+
+    private config: Pick<GraphConfig, 'arrowColor' | 'arrowSize' | 'lineHitWidth' | 'nodeRadius' | 'selfLinkDistance' | 'selfLinkRadius'>;
+    private layout: Layout<Graph>;
+    private hitAreaPolygon: Polygon;
+    private _selfLink: boolean;
+
+    /**
+     *
+     */
+    constructor(graphLink: Link, layout: Layout<Graph>, config: GraphConfig) {
+        super();
+        this.id = graphLink.data.groupId;
+        this.layout = layout;
+        this.config = (({ arrowColor, arrowSize, lineHitWidth, nodeRadius, selfLinkDistance, selfLinkRadius }) => ({ arrowColor, arrowSize, lineHitWidth, nodeRadius, selfLinkDistance, selfLinkRadius }))(config);
+        this._selfLink = graphLink.fromId === graphLink.toId;
+        this.sourceLinks = [];
+        this.edges = {};
+        this.interactive = true;
+        this.buttonMode = true;
+        this.hitAreaPolygon = new Polygon();
+        this.hitArea = this.hitAreaPolygon;
+    }
+
+    get selfLink() {
+        return this._selfLink;
+    }
+
+    addGraphLink(graphLink: Link): void {
+        if (graphLink.data.groupId !== this.id) throw new Error('Link must be added to the same group');
+        this.sourceLinks.push(graphLink.data.uuid);
+        this.edges[graphLink.id] = {
+            position: this.layout.getLinkPosition(graphLink.id)
+        }
+    }
+
+    /**
+     * Removes a {@link Link} from the graphic. Also removes the graphic from the container if there are no more edges to be rendered.
+     * @param graphLink the link to be removed
+     * @returns true if there are no more edges to render, false otherwise
+     */
+    removeGraphLink(graphLink: Link): boolean {
+        const link = this.sourceLinks.findIndex(l => l === graphLink.data.uuid);
+        if (link >= 0) {
+            this.sourceLinks.splice(link, 1);
+            // if there are no more links with the same id (same from-to), remove the edge
+            if (!this.sourceLinks.includes(graphLink.id))
+                delete this.edges[graphLink.id];
+
+            if (!Object.keys(this.edges).length) {
+                //no more edges, destroy graphic
+                this.parent.removeChild(this);
+                this.destroy({ children: true });
+                // this.links.delete(graphLink.data.groupId); // TODO: remove from control list
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    update(): void {
+        this.calculateEdges();
+        this.drawEdges();
+    }
+
+    protected calculateEdges(): void {
+        let hitAreaCalculated = false;
+        for (const k in this.edges) {
+            const edge = this.edges[k];
+            
+            // get source coords
+            let from: Vector = {
+                x: edge.position.from.x,
+                y: edge.position.from.y,
+            };
+
+            // alternate source coords calc if link to itself
+            if (this.selfLink) {
+                if (this.fromPos) {
+                    from = this.fromPos;
+                } else {
+                    this.fromPos = from;
+                }
+                from.x = edge.position.to.x - this.config.selfLinkDistance;
+                from.y = edge.position.to.y;
+            }
+
+            // calculate end coords
+            // TODO: calculate based on node shape?
+            const angle = Math.atan2(edge.position.to.y - from.y, edge.position.to.x - from.x);
+            const end: Vector = {
+                x: -Math.cos(angle) * this.config.nodeRadius + edge.position.to.x,
+                y: -Math.sin(angle) * this.config.nodeRadius + edge.position.to.y,
+            };
+
+            // calculate hitArea only once
+            if (!hitAreaCalculated) {
+                let offset = this.config.nodeRadius;
+                if (this.selfLink) {
+                    offset = 0;
+                }
+                const traverseAngle = angle - (90 * Math.PI) / 180;
+                const sizeX = Math.cos(traverseAngle) * this.config.lineHitWidth;
+                const sizeY = Math.sin(traverseAngle) * this.config.lineHitWidth;
+                const start = {
+                    x: Math.cos(angle) * offset + from.x,
+                    y: Math.sin(angle) * offset + from.y,
+                };
+                this.hitAreaPolygon.points.splice(0);
+                this.hitAreaPolygon.points.push(
+                    start.x + sizeX,
+                    start.y + sizeY, //p1
+                    end.x + sizeX,
+                    end.y + sizeY, //p2
+                    end.x - sizeX,
+                    end.y - sizeY, //p3
+                    start.x - sizeX,
+                    start.y - sizeY //p4
+                );
+                hitAreaCalculated = true; // TODO: check
+            }
+
+            // calculate arrow points
+            edge.arrow = {
+                start: end,
+                nodes: [
+                    {
+                        x: -Math.cos(angle - (20 * Math.PI) / 180) * this.config.arrowSize + end.x,
+                        y: -Math.sin(angle - (20 * Math.PI) / 180) * this.config.arrowSize + end.y,
+                    },
+                    {
+                        x: -Math.cos(angle + (20 * Math.PI) / 180) * this.config.arrowSize + end.x,
+                        y: -Math.sin(angle + (20 * Math.PI) / 180) * this.config.arrowSize + end.y,
+                    },
+                ],
+            };
+        }
+    }
+
+    protected drawEdges(): void {
+        this.clear();
+        let lineDrawn = false;
+        for (const k in this.edges) {
+            const edge = this.edges[k];
+
+            // draw line only once
+            if (!lineDrawn) {
+                if (!this.fromPos) {
+                    this.moveTo(edge.position.from.x, edge.position.from.y);
+                } else {
+                    this.moveTo(this.fromPos.x, this.fromPos.y);
+                }
+                this.lineStyle(1, 0x000000, 0.3);
+                this.lineTo(edge.position.to.x, edge.position.to.y);
+                if (this.selfLink && this.fromPos) {
+                    this.lineStyle(0);
+                    this.beginFill(this.config.arrowColor);
+                    this.drawCircle(this.fromPos.x, this.fromPos.y, this.config.selfLinkRadius);
+                }
+                lineDrawn = true;
+            }
+
+            // draw arrow
+            if (edge.arrow) {
+                this.moveTo(edge.arrow.start.x, edge.arrow.start.y);
+                this.lineStyle(0);
+                this.beginFill(this.config.arrowColor);
+                edge.arrow.nodes.forEach(node => {
+                    this.lineTo(node.x, node.y);
+                });
+            }
+
+            this.closePath();
+            this.endFill();
+        }
+    }
+}
---
+++ src/components/pixi-node.ts
@@ -0,0 +1,180 @@
+import { Container, Graphics, InteractionEvent, Sprite, Text, Texture } from "@/plugins/pixijs";
+import { Graph, Node, NodeId, Layout, Vector } from "@/plugins/ngraph";
+import { GraphConfig } from "@/options";
+
+export class PixiNode extends Container {
+    graphPosition: Vector;
+    label?: string;
+    nodeId: NodeId;
+    target: boolean;
+
+    private config: GraphConfig;
+    private layout: Layout<Graph>;
+    private isDragging = false;
+    private dragEnded = false;
+    private _shouldTap = false;
+
+    constructor(graphNode: Node, layout: Layout<Graph>, config: GraphConfig, texture?: Texture) {
+        super();
+        this.config = { ...config };
+        this.label = graphNode.data.label;
+        this.nodeId = graphNode.id;
+        this.target = graphNode.data.target || false;
+        this.layout = layout;
+        this.graphPosition = layout.getNodePosition(graphNode.id);
+
+        // Create shape, label and icon and add them to the container
+        
+        const shape = this.createShape();
+        this.addChild(shape);
+
+        const label = this.createLabel();
+        label && this.addChild(label);
+        
+        if (texture) {
+            const icon = this.createIcon(texture);
+            this.addChild(icon);
+        }
+    }
+
+    get shouldTap(): boolean {
+        return this._shouldTap;
+    }
+
+    updatePosition(): void {
+        this.setTransform(this.graphPosition.x, this.graphPosition.y);
+    }
+
+    isPinned(): boolean {
+        const node = this.layout.graph.getNode(this.nodeId);
+        if (node) {
+            return this.layout.isNodePinned(node);
+        }
+        return false;
+    }
+
+    pin(): void {
+        const node = this.layout.graph.getNode(this.nodeId);
+        node && this.layout.pinNode(node, true);
+    }
+
+    onDragStart(e: InteractionEvent): void {
+        if (e.data.button == 0) {
+            this.dragEnded = false;
+            setTimeout(() => {
+                if (!this.dragEnded) {
+                    this.pin();
+                    this.isDragging = true;
+                    this._shouldTap = false;
+                }
+            }, 200);
+        }
+    }
+
+    onDragEnd(_: InteractionEvent): void {
+        this.dragEnded = true;
+        if (!this.isDragging) {
+            this._shouldTap = true;
+        } else {
+            this.isDragging = false;
+        }
+    }
+
+    onDragMove(e: InteractionEvent): boolean {
+        if (this.isDragging) {
+            const graphPos = e.data.getLocalPosition(this.parent, undefined, e.data.global); // pixi.getGraphCoordinates(e.data.global.x, e.data.global.y);
+            this.layout.setNodePosition(this.nodeId, graphPos.x, graphPos.y);
+            return true;
+        }
+        return false;
+    }
+
+    // Node creation methods
+
+    /**
+     * Creates a basic shape to represent a node.
+     * 
+     * Supported shapes are circle, square and roundSquare.
+     * @returns a {@link Graphics} object that represents a node to be rendered
+     */
+    protected createShape(): Graphics {
+        const shape = new Graphics();
+        const halfNode = this.config.nodeSize / 2;
+
+        // if node is a target, color should be different
+        shape.beginFill(this.target ? this.config.targetColor : this.config.nodeColor);
+        switch (this.config.shape) {
+            case "circle":
+                shape.drawCircle(0, 0, halfNode);
+                break;
+
+            case "square":
+                shape.drawRect(-halfNode, -halfNode, this.config.nodeSize, this.config.nodeSize);
+                break;
+
+            case "roundSquare":
+                shape.drawRoundedRect(-halfNode, -halfNode, this.config.nodeSize, this.config.nodeSize, this.config.nodeSize * 0.1);
+                break;
+
+            default:
+                break;
+        }
+
+        shape.endFill();
+        return shape;
+    }
+
+    /**
+     * Creates a node label to be rendered inside the shape
+     * @returns the {@link Text} to be rendered
+     */
+    protected createLabel(): Text | undefined {
+        if (!this.label) return;
+        let nodeText: Text;
+
+        // Apply text transformation to the label if provided in the config
+        const _label = this.config.labelTransform ? this.config.labelTransform(this.label) : this.label;
+        if (!_label.length) return;
+
+        // Create the Text object. TODO: Drop shadow and stroke values should be customizable?
+        nodeText = new Text(_label, {
+            fontFamily: this.config.fontFamily,
+            fontSize: this.config.fontSize,
+            fill: this.config.fontColor,
+            align: "center",
+            dropShadow: true,
+            dropShadowDistance: 2,
+            strokeThickness: 1,
+            stroke: this.target ? this.config.targetColor : this.config.fontStrokeColor,
+        });
+        
+        // Increase default text resolution
+        nodeText.resolution = 2;
+
+        return nodeText;
+    }
+
+    /**
+     * Creates an icon to be rendered inside the shape
+     * @param texture the source image for the icon generation
+     * @returns the {@link Sprite} to be rendered
+     */
+    protected createIcon(texture: Texture): Sprite {
+        const icon = Sprite.from(texture);
+        icon.width = this.config.nodeIconSize;
+        icon.height = this.config.nodeIconSize;
+        icon.anchor.set(0.5);
+        
+        // change icon position if there is a label
+        if (this.children.find(c => c instanceof Text)) {
+            icon.y = -this.config.nodeSize / 4.5;
+        }
+
+        // change texture color if config specified
+        if (this.config.nodeIconColor) {
+            icon.tint = this.config.nodeIconColor;
+        }
+
+        return icon;
+    }
+}
